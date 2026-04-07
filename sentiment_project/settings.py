4dc2afdd3b1a9bb7bb8f0b8a0cddd7cf03ec7d7d
megha <meghaaji02@gmail.com>
2026-04-07 15:16:25 +0530

    Deployment ready

--- sentiment_project/settings.py
+++ sentiment_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-*l55gw$&9%hqvm6_hze+q*+7rwvof**mt*z871ab^8h=ncgs*q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ start.sh
@@ -0,0 +1,1 @@
+gunicorn sentiment_project.wsgi:application
